6bff91077f945cfb2fb187484edbdef99687ad28
SethDocherty <sddocherty@gmail.com>
2025-09-22 16:56:17 -0400

    Updates documentation configuration and python package versions
    Refactors the documentation configuration for improved functionality and appearance.
    
    This includes:
    - Updates copyright year to 2025.
    - Adds new extensions and configuration options for enhanced features.
    - Modifies exclude patterns to exclude unnecessary files/folders.
    - Updates HTML theme options for improved user experience including updated light/dark logo.
    - Adds support for clickable images with `sphinxcontrib-images`.
    - Updates dependencies and adds new dependencies in `requirements.txt`.

--- conf.py
+++ conf.py
@@ -3,15 +3,32 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# import ablog
+
+# Compatibility shim: avoid Theme.get_config raising and returning nothing for non-[theme] sections
+try:
+    from sphinx.theming import Theme
+    _orig_get_config = Theme.get_config
+
+    def _safe_get_config(self, section, key=None, default=None):
+        try:
+            return _orig_get_config(self, section, key, default)
+        except Exception:
+            # If theme implementation changed or Sphinx raises for unknown sections,
+            # return the provided default so templates get sensible fallbacks
+            return default
+
+    Theme.get_config = _safe_get_config
+except Exception:
+    # keep going if theming API is different / unavailable
+    pass
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-import ablog
-
-project = 'EASIER Website'
-copyright = '2023, The EASIER Data Initiative'
-author = 'The EASIER Data Initiative'
-release = '1.0'
+project = "EASIER Website"
+copyright = "2025, The EASIER Data Initiative"
+author = "The EASIER Data Initiative"
+release = "1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -22,35 +39,59 @@ extensions = [
     "sphinx_panels",
     "myst_parser",
     "nbsphinx",
+    "sphinxcontrib.images",
+    "sphinx_new_tab_link",
+    "sphinx_subfigure"
 ]
+    # "sphinxcontrib.mermaid",
 
-# Markdown support
+language = "en"
+
+# Add any paths that contain templates here, relative to root directory of repo
+templates_path = ["_templates"]
+
+# -- MyST related params -------------------------------------------------
+myst_enable_extensions = ["html_image", "attrs_inline", "colon_fence"]
 myst_update_mathjax = False
+# myst_fence_as_directive = ["mermaid"]
+suppress_warnings = ["myst.header"] # Markdown support
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This patterns also effect to html_static_path and html_extra_path
 exclude_patterns = [
-    "posts/*/.ipynb_checkpoints/*",
+    "**.ipynb_checkpoints**",
     ".github/*",
     ".history",
     "github_submodule/*",
     "LICENSE.md",
     "./README.md",
+    "**.doctrees**",
+    "**_website**",
+    "**.venv**",
+    "_build",
+    "Thumbs.db",
+    ".DS_Store"
 ]
-
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
-html_title = 'The EASIER Data Initiative'
-
+html_theme = "pydata_sphinx_theme"
+html_title = "The EASIER Data Initiative"
 html_show_sourcelink = False
-html_static_path = ['_static']
+html_static_path = ["_static"]
+html_extra_path = ["_img"]
 html_logo = "_static/logo.png"
-html_css_files = [
-    "css/custom.css"
-]
-html_js_files = ['custom.js']
+html_css_files = ["css/custom.css"]
+html_js_files = ["hideTop.js"]
+html_context = {"default_mode": "light", "logo": "_static/logo.png"}
 
 html_sidebars = {
     "**": ["index.html"],
@@ -58,47 +99,47 @@ html_sidebars = {
 
 html_theme_options = {
     "secondary_sidebar_items": ["page-toc", "edit-this-page", "sourcelink"],
-    "footer_items": ["copyright"],
+    "content_footer_items": [],
+    "footer_start": ["copyright", "last-updated"],
+    "footer_end": [],
     "show_toc_level": 2,
-    "favicons": [
-        {
-            "rel": "icon",
-            "sizes": "16x16",
-            "href": "favicon.png"
-        },
-        {
-            "rel": "icon",
-            "sizes": "32x32",
-            "href": "favicon.png"
-        },
-        {
-            "rel": "icon",
-            "sizes": "180x180",
-            "href": "favicon.png"
-        }
-    ]
+    "logo": {
+      "light_image": "_static/logo.png",
+      "dark_image": "_static/logo_black.png",
+   }
 }
 
 html_meta = {
-    'description': 'The EASIER Data Initiative develops decentralized cyberinfrastructure for efficiently, accessibly, and sustainably onloading, analyzing, and extracting large amounts of geospatial data. Concerning onloading, we are building a generalizable pipeline for heterogeneous geospatial data ingress into the Filecoin and IPFS environment.',
-    'og:title': 'The EASIER Data Initiative',
-    'og:description': 'The EASIER Data Initiative develops decentralized cyberinfrastructure for efficiently, accessibly, and sustainably onloading, analyzing, and extracting large amounts of geospatial data. Concerning onloading, we are building a generalizable pipeline for heterogeneous geospatial data ingress into the Filecoin and IPFS environment.',
-    'og:image': 'https://pbs.twimg.com/profile_images/1574844893817491457/sWuj_YTp_400x400.jpg',
-    'twitter:title': 'The EASIER Data Initiative',
-    'twitter:description': 'The EASIER Data Initiative develops decentralized cyberinfrastructure for efficiently, accessibly, and sustainably onloading, analyzing, and extracting large amounts of geospatial data. Concerning onloading, we are building a generalizable pipeline for heterogeneous geospatial data ingress into the Filecoin and IPFS environment.',
-    'twitter:image': 'https://pbs.twimg.com/profile_images/1574844893817491457/sWuj_YTp_400x400.jpg',
+    "description": "The EASIER Data Initiative develops decentralized cyberinfrastructure for efficiently, accessibly, and sustainably onloading, analyzing, and extracting large amounts of geospatial data. Concerning onloading, we are building a generalizable pipeline for heterogeneous geospatial data ingress into the Filecoin and IPFS environment.",
+    "og:title": "The EASIER Data Initiative",
+    "og:description": "The EASIER Data Initiative develops decentralized cyberinfrastructure for efficiently, accessibly, and sustainably onloading, analyzing, and extracting large amounts of geospatial data. Concerning onloading, we are building a generalizable pipeline for heterogeneous geospatial data ingress into the Filecoin and IPFS environment.",
+    "og:image": "https://pbs.twimg.com/profile_images/1574844893817491457/sWuj_YTp_400x400.jpg",
+    "twitter:title": "The EASIER Data Initiative",
+    "twitter:description": "The EASIER Data Initiative develops decentralized cyberinfrastructure for efficiently, accessibly, and sustainably onloading, analyzing, and extracting large amounts of geospatial data. Concerning onloading, we are building a generalizable pipeline for heterogeneous geospatial data ingress into the Filecoin and IPFS environment.",
+    "twitter:image": "https://pbs.twimg.com/profile_images/1574844893817491457/sWuj_YTp_400x400.jpg",
 }
 
-html_context = {
-    "default_mode": "light"
-}
 
+# -- Blog Page Confuguration -------------------------------------------------
 blog_feed_fulltext = False
-
 blog_post_pattern = "updates/*/*"
-blog_path = "updates"
-
-suppress_warnings = ["myst.header"]
-
-exclude_patterns = []
+blog_path = "./updates"
 jupyter_execute_notebooks = "off"
+
+
+# -- sphinxcontrib-images configuration ----------------------------------------------
+# Configure sphinxcontrib-images for clickable images
+# See Documentation: https://sphinxcontrib-images.readthedocs.io/en/latest
+# also this article: https://dev.to/crispy-broccoli/sphinx-inserting-media-in-restructured-rest-files-2amn
+
+images_config = {
+    "backend": "LightBox2",
+    "default_image_width": "100%",
+    "default_show_title": "True",
+    "default_group": "default",
+}
+
+# -- Ablog properties -------------------------------------------------
+# Configure directories/patterns to watch for rebuilds during ablog serve -r
+# ablog_rebuild_dirs = ["./updates/"]
+
